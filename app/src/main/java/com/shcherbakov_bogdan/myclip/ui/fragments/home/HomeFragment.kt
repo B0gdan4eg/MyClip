f6a7cbffdab718f96dedcde4295fd52b8a124112
B0gdan4eg <shcherbakov.b@mail.ru>
2022-10-16 12:30:37 +0300

    fab menu

--- app/src/main/java/com/shcherbakov_bogdan/myclip/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/shcherbakov_bogdan/myclip/ui/fragments/home/HomeFragment.kt
@@ -1,22 +1,34 @@
 package com.shcherbakov_bogdan.myclip.ui.fragments.home
 
 import android.os.Bundle
+import android.support.v4.media.session.MediaSessionCompat.Token.fromBundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.app.Person.fromBundle
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.media.AudioAttributesCompat.fromBundle
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.shcherbakov_bogdan.myclip.R
 import com.shcherbakov_bogdan.myclip.data.transactions.Transactions
 import com.shcherbakov_bogdan.myclip.databinding.FragmentHomeBinding
+import com.shcherbakov_bogdan.myclip.ui.dialogs.TransactionDialogFragment
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
 class HomeFragment : DaggerFragment() {
 
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.rotate_open_anim) }
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.rotate_close_anim) }
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.from_bottom_anim) }
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(context,R.anim.to_bottom_anim) }
+
+    private var clicked = false
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var adapter: HomeListAdapter
@@ -32,8 +44,52 @@ class HomeFragment : DaggerFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater,container,false)
-        binding.floatingActionButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.transactionDialogFragment, null))
+        binding.fabFirst
+        binding.fabSecond.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.transactionDialogFragment, null))
+        binding.fabMain.setOnClickListener {
+            onAddButtonClicked()
+        }
         return binding.root
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        setClickable(clicked)
+        clicked = !clicked
+    }
+
+    private fun setAnimation(clicked : Boolean) {
+        if (!clicked) {
+            fab_first.startAnimation(fromBottom)
+            fab_second.startAnimation(fromBottom)
+            fab_main.startAnimation(rotateOpen)
+        } else {
+            fab_first.startAnimation(toBottom)
+            fab_second.startAnimation(toBottom)
+            fab_main.startAnimation(rotateClose)
+        }
+
+    }
+
+    private fun setVisibility(clicked : Boolean) {
+        if (!clicked) {
+            fab_first.visibility = View.VISIBLE
+            fab_second.visibility = View.VISIBLE
+        } else {
+            fab_first.visibility = View.INVISIBLE
+            fab_second.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setClickable(clicked: Boolean) {
+        if (clicked) {
+            fab_first.isClickable = false
+            fab_second.isClickable = false
+        } else {
+            fab_first.isClickable = true
+            fab_second.isClickable = true
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
