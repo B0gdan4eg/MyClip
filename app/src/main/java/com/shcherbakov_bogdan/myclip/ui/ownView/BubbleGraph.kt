fa0352ab1208649023192ba501a477879fee7407
Bogdan Shcherbakov <101798906+B0gdan4eg@users.noreply.github.com>
2022-11-03 23:52:01 +0300

    fix something

--- app/src/main/java/com/shcherbakov_bogdan/myclip/ui/ownView/BubbleGraph.kt
+++ app/src/main/java/com/shcherbakov_bogdan/myclip/ui/ownView/BubbleGraph.kt
@@ -6,27 +6,34 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.graphics.setBlendMode
 import com.shcherbakov_bogdan.myclip.R
 import com.shcherbakov_bogdan.myclip.utils.pack
 
 class BubbleGraph : View {
 
     constructor(context: Context) : super(context) {}
-    constructor(context: Context, attrs : AttributeSet) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.BubbleGraph)
-        circleFill?.color = typedArray.getColor(R.styleable.BubbleGraph_circle_fill_color,Color.BLUE)
-        circleStroke?.color = typedArray.getColor(R.styleable.BubbleGraph_circle_border_color,Color.BLUE)
+        circleFill?.color =
+            typedArray.getColor(R.styleable.BubbleGraph_circle_fill_color, Color.BLUE)
+        circleStroke?.color =
+            typedArray.getColor(R.styleable.BubbleGraph_circle_border_color, Color.BLUE)
         typedArray.recycle()
     }
-    constructor(context: Context, attrs : AttributeSet, defStyleAttr : Int) : super(context, attrs, defStyleAttr) {}
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+    }
 
     private var circleFill: Paint? = null
     private var circleStroke: Paint? = null
-    private var args : List<Double> = listOf(10.0 ,10.0 ,10.0)
+    private var args: List<Double> = listOf(10.0, 10.0, 10.0)
     private var cx: Float = 0f
     private var cy: Float = 0f
-    private var strokeWidth : Int = 10
+    private var strokeWidth: Int = 10
     private var minVSize: Int = 0
 
     init {
@@ -37,22 +44,34 @@ class BubbleGraph : View {
         circleFill?.color = Color.GREEN
         circleFill?.flags = Paint.ANTI_ALIAS_FLAG
     }
+
     override fun onDraw(canvas: Canvas?) {
         setMeasuredDimension(cx.toInt(), cy.toInt())
         args = listOf(
             150.0, 50.0, 70.0, 120.0
-            )
+        )
         val floatList = pack(args)
         for (args in floatList) {
-            canvas?.drawCircle(args.first.toFloat() + cx, args.second.toFloat() + cy, args.third.toFloat(), circleStroke!!)
+            canvas?.drawCircle(
+                args.first.toFloat() + cx,
+                args.second.toFloat() + cy,
+                args.third.toFloat(),
+                circleStroke!!
+            )
         }
         for (args in floatList) {
-            canvas?.drawCircle(args.first.toFloat() + cx, args.second.toFloat() + cy, args.third.toFloat() - strokeWidth, circleFill!!)
+            canvas?.drawCircle(
+                args.first.toFloat() + cx,
+                args.second.toFloat() + cy,
+                args.third.toFloat() - strokeWidth,
+                circleFill!!
+            )
         }
 
 
         super.onDraw(canvas)
     }
+
     private fun decodeMeasureSpec(measureSpec: Int): Int {
         val result: Int
         val specMode = MeasureSpec.getMode(measureSpec)
@@ -66,7 +85,8 @@ class BubbleGraph : View {
         val mWidth = decodeMeasureSpec(widthMeasureSpec) //Достаём размер View
         val mHeight = decodeMeasureSpec(heightMeasureSpec)
 
-        minVSize = mWidth.coerceAtMost(mHeight) //Вычисляем минимальный размер (будем рисовать круги по минимальному размеру View в высоте или ширине)
+        minVSize =
+            mWidth.coerceAtMost(mHeight) //Вычисляем минимальный размер (будем рисовать круги по минимальному размеру View в высоте или ширине)
 
         setMeasuredDimension(mWidth, mHeight) //Сохраненяем измеренную ширину и высоту для View
 
