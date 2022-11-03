0f28abee7b96dfe104bdb7f7c590906f21101146
Bogdan Shcherbakov <101798906+B0gdan4eg@users.noreply.github.com>
2022-11-03 23:48:04 +0300

    fix something

--- app/src/main/java/com/shcherbakov_bogdan/myclip/data/account/AccountDao.kt
+++ app/src/main/java/com/shcherbakov_bogdan/myclip/data/account/AccountDao.kt
@@ -1,6 +1,8 @@
 package com.shcherbakov_bogdan.myclip.data.account
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface AccountDao {
